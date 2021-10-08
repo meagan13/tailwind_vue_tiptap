783f534430a81280147184a71b0129ef8d854501
meagan <meagan.h.smith@gmail.com>
2021-10-08 18:06:19 -0500

    Initial commit

---
+++ src/components/Tiptap.vue
@@ -0,0 +1,28 @@
+<template>
+  <editor-content class="bg-green-900" :editor="editor" />
+  <div class="w-100 bg-pink-800 text-white">
+      <h4 class="text-center py-5">Meagan Test</h4>
+  </div>
+</template>
+
+<script>
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+export default {
+  components: {
+    EditorContent,
+  },
+
+  setup() {
+    const editor = useEditor({
+      content: '<p>I’m running tiptap with Vue.js. 🎉</p>',
+      extensions: [
+        StarterKit,
+      ],
+    })
+
+    return { editor }
+  },
+}
+</script>
